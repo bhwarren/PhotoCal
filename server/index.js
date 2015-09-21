5663083a7f56d2bb8524cc7a855e6158b6a99406
bhwarren <bhwarren@users.noreply.github.com>
2015-09-21 00:08:45 -0400

    working upload api & OCR response

--- server/index.js
+++ server/index.js
@@ -2,69 +2,159 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var spawn = require('child_process').spawn;
 var _ = require('underscore');
-var sh = require('execSync');
+var formidable = require('formidable');
+var util = require('util');
+var http = require('http');
 
 var app = express();
 app.use(bodyParser.json({limit: '50mb'}));
 
-var port = process.argv[2] || 8008;
+var port = process.argv[2] || 8088;
 
 var server = app.listen(port, function () {
-		var host = server.address().address;
-		var port = server.address().port;
-		console.log('Processor listening at http://%s:%s', host, port);
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('Processor listening at http://%s:%s', host, port);
+});
 
-		var nerStatus = sh.exec('ps -ef |grep NERServer | grep -v "grep"').stdout;
-		if(nerStatus == ""){
-			//start the java server
-			console.log("starting java ner server");
-			var serverProc = spawn('java', ['-mx700m','-cp','stanford-ner-2015-04-20/stanford-ner-with-classifier.jar','edu.stanford.nlp.ie.NERServer','-port','9191','-loadClassifier','stanford-ner-2015-04-20/classifiers/english.muc.7class.distsim.crf.ser.gz']);
+app.get('/', function (req, res){
+  res.writeHead(200, {'Content-Type': 'text/html' });
+  var form = '<form action="/upload" enctype="multipart/form-data" method="post">Add a title: <input name="title" type="text" /><br><br><input multiple="multiple" name="upload" type="file" /><br><br><input type="submit" value="Upload" /></form>';
+  res.end(form); 
+}); 
+
+//when a picture is posted, get the calendar content
+app.post('/upload', function(req, res) {
+	var form = new formidable.IncomingForm();
+
+	form.parse(req, function(err, fields, files) {
+	    	res.writeHead(200, {'content-type': 'text/plain'});
+	});
+	form.on('file', function(name, file) {
+		var picture_path = this.openedFiles[0].path; 
+	    	var filename = this.openedFiles[0].name;
+
+		//move the picture to a more appropriate place
+		var fs = require('fs');
+	        var target_path = './uploads/' + filename;
+	        fs.rename(picture_path, target_path, function(err) {
+	        	if (err) throw err;
+	         	console.log("Upload completed, now doing OCR");
+	        });
+	
+		//#do the OCR
+		var text_path = "./uploads/"+filename;
+		var ocr_proc = spawn('tesseract', [target_path, text_path, "-psm", "1"]);
+	        ocr_proc.on('close', function (code) {
+	                console.log('tesseract exited with code ' + code);
+	
+			//remove the picture file to keep space free
+			/*fs.unlink(target_path, function(err){
+				if(err) throw err;
+	         		console.log("successfully removed picture");
+			});
+			*/
+	
+			if(code != 0){
+				console.log("tesseract exited oddly, cancelling");
+				return;
+			}	
+	
+			//if we get here, then we have text data to send to the entity manager 
+			text_path = text_path + ".txt";
+			fs.readFile(text_path, 'utf8', function (err,data) {
+				if(err) throw err;
+			  	console.log("sending this data to be analyzed: "+data);
+	
+				//send the data to the entity manager
+				//var output = JSON.stringify(tag_entities(data));
+
+			        var json_data = JSON.stringify({
+			                "file": data,
+			                "port": "9191"
+			        });
+			
+			
+			        // An object of options to indicate where to post to
+			        var post_options = {
+			                host: 'localhost',
+			                port: '8008',
+			                path: '/ner',
+			                method: 'POST',
+			                headers: {
+			                        'Content-Type': 'application/json',
+			                }
+			        };
+			
+			        // Set up the request
+			        var post_req = http.request(post_options, function(post_res) {
+			                post_res.setEncoding('utf8');
+			                post_res.on('data', function (response) {
+			                        console.log('Response: ' + response);
+			                        //res.write(response);
+						res.end(response);
+			                });
+			        });
+			
+			        // post the data
+			        post_req.write(json_data);
+			        post_req.end();
+
+	
+				//remove the generated ocr file
+				/*fs.unlink(text_path, function(err){
+	                        	if(err) throw err;
+	                        	console.log("successfully removed text file");
+	                	});
+				*/
+			});
+							
+		});
+	
+		
+		//#str="Friday, March 14, 2008 MIT’s Stata Center 32 Vassar St, Cambridge, MA 32-124 10:30am-12pm"
+		
+		//curl -H "Content-Type: application/json" -X POST -d "{\"file\":\"$str\",\"port\":\"9191\"}" http://localhost:8008/ner
+		//curl -H "Content-Type: application/json" -X POST -d {"file":"$str","port":"9191"} http://localhost:8008/ner
+	
+	
+		/*process.stdout.on('data', function (data) { });
+		process.stdin.on('endData',function (data){ })
+		process.stderr.on('data', function (data) { });
+		process.on('close', function (code) { });
+		*/
+	});
+});
+
+function tag_entities(raw_text) {
+
+	var json_data = JSON.stringify({
+		"file": raw_text,
+		"port": "9191"
+	});
+
+
+	// An object of options to indicate where to post to
+	var post_options = {
+		host: 'localhost',
+		port: '8008',
+		path: '/ner',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
 		}
-});
+	};
 
-app.get('/', function (req, res) {
-		res.send('how dare you\n');
-});
-
-app.get('/ner', function (req, res) {
-		res.send('how dare you\n');
-});
-
-app.post('/ner', function(req, res) {
-		var parsed = '';
-		var nerPort = req.body.port ? req.body.port : 9191;
-		var text = req.body.file.replace(/\n+/gm, function myFunc(x){return' ';});
-
-		//when java server returns data
-		var process = spawn('java', ['-cp', 'stanford-ner-2015-04-20/stanford-ner-with-classifier.jar', 'edu.stanford.nlp.ie.NERServer' ,'-port','9191','-client']);
-		process.stdout.on('data', function (data) {
-				//ignore if 'Input' write file text to stream
-				if(String(data).indexOf('Input some text and press RETURN to NER tag it,  or just RETURN to finish.')==0){
-						process.stdin.write(text);
-						process.stdin.write('\n');
-						process.stdin.write('\n');
-						return;
-				}
-				//concat returned data
-				else if(String(data).length > 1){
-						parsed += String(data);
-						return;
-				}
+	// Set up the request
+	var post_req = http.request(post_options, function(post_res) {
+		post_res.setEncoding('utf8');
+		post_res.on('data', function (response) {
+			console.log('Response: ' + response);
+			res.write(response);
 		});
+	});
 
-		process.stdin.on('endData',function (data){
-				console.log('endData: '+data);
-		})
-
-		process.stderr.on('data', function (data) {
-		  console.log('stderr: ' + data);
-		});
-
-		//when process ends
-		process.on('close', function (code) {
-				console.log('stanford-ner process exited with code ' + code);
-				//return ner tags, after parsing
-				//res.status(200).json({entities:parse(parsed)});
-		});
-
-});
+	// post the data
+	post_req.write(json_data);
+	post_req.end();
+}
