50a4aa418de884817b34c146fc31761dab4e675b
bhwarren <bhwarren@users.noreply.github.com>
2015-09-20 20:21:30 -0400

    initial photo server commit

---
+++ server/ocr-analysis.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#the first argument needs to be the picture of the flyer
+
+#remove the extension, and remove the preceding directories to get filename
+filename=`echo $1 | sed 's/\.[^\.]*$//' | awk -F "/" '{print $NF}'`
+
+#do the OCR, store in tmp
+tesseract "$1" "/tmp/$filename" -psm 1
+
+#str="Friday, March 14, 2008 MIT’s Stata Center 32 Vassar St, Cambridge, MA 32-124 10:30am-12pm"
+#str="dasdfas asdfasgfd"
+
+str=`cat "/tmp/$filename"`
+
+#remove the picture and the OCR text now we have in $str
+rm "/tmp/$filename"
+rm "$1"
+
+echo -e "$str"
+
+curl -H "Content-Type: application/json" -X POST -d "{\"file\":\"$str\",\"port\":\"9191\"}" http://localhost:8008/ner
